8ddb971f373b0a503e4182e225e5a8bfa6a2276c
bcollins <parietal.io@gmail.com>
2018-08-07 10:32:03 -0400

    added logging module

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -96,4 +96,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
+
 django_heroku.settings(locals())
