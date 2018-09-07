db2a971cea99e5d528a50bd074c1dbb84a2a20b1
brendancol <parietal.io@gmail.com>
2018-09-07 15:28:19 -0500

    added an integration test for index, auth, and auth_callback views

--- deauthorized/tests.py
+++ deauthorized/tests.py
@@ -1,18 +1,69 @@
-from django.contrib.auth.models import AnonymousUser, User
+from unittest import mock
+
 from django.test import TestCase, RequestFactory
 
 from .views import index
+from .views import auth
+from .views import auth_callback
+
+
+class MockResponse:
+
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
+
+    def raise_for_status(self):
+        pass
+
+
+def mocked_requests_get(*args, **kwargs):
+    requested_url = args[0]
+    if 'op/me' in requested_url:
+        resp = dict(name='test',
+                    nickname='test',
+                    email='test',
+                    email_verified='test',
+                    updated_at='test')
+        return MockResponse(resp, 200)
+    else:
+        raise ValueError('Not sure how to mock url:{}'.format(requested_url))
+
+
+def mocked_requests_post(*args, **kwargs):
+
+    requested_url = args[0]
+    if 'token' in requested_url:
+        resp = dict(access_token='fill in access token',
+                    id_token='fill in id token')
+        return MockResponse(resp, 200)
+    else:
+        raise ValueError('Not sure how to mock url:{}'.format(requested_url))
+
 
 class SimpleTest(TestCase):
+
     def setUp(self):
-        # Every test needs access to the request factory.
         self.factory = RequestFactory()
 
-    def test_details(self):
-        # Create an instance of a GET request.
+    def test_index_view(self):
         request = self.factory.get('/')
-        request.user = AnonymousUser()
-
-        # Test my_view() as if it were deployed at /customer/details
         response = index(request)
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
+
+    def test_auth_view(self):
+        request = self.factory.get('/auth')
+        response = auth(request)
+        assert response.status_code == 302
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    @mock.patch('requests.post', side_effect=mocked_requests_post)
+    def test_auth_callback_view(self, mock_post, mock_get):
+        request_params = {'code': 'test', 'state': 'test'}
+        request = self.factory.get('/openid_auth_callback', request_params)
+        response = auth_callback(request)
+        assert response.status_code == 200
+        assert 'text/html' in response.get('content-type')
